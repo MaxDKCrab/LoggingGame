e0c7f16557fd797f1e3b3a6b4ebb6a76f501c638
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-05-13 19:57:00 +0200

    Jump and sprint for lambert

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -5,24 +5,46 @@ using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
     [SerializeField] private Transform camera;
     [SerializeField] private CharacterController charControll;
     [SerializeField] private float speed;
+    [SerializeField] private float sprintSpeed;
     [SerializeField] private float turnSmoothTime;
+    [SerializeField] private float jumpForce;
     private float turnSmoothVelocity;
     private Animator anim;
+    private bool isGrounded;
+    private Vector3 velocity;
+    
 
     private void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y += jumpForce;
+        }
+        
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
@@ -30,12 +52,29 @@ public class ThirdPersonController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f,angle,0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            charControll.Move(moveDirection.normalized * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                charControll.Move(moveDirection.normalized * sprintSpeed * Time.deltaTime);
+            }
+            else
+            {
+                charControll.Move(moveDirection.normalized * speed * Time.deltaTime);
+            }
+           
             anim.SetBool("IsMove",true);
         }
         else
         {
             anim.SetBool("IsMove",false);
         }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        charControll.Move(velocity * Time.deltaTime);
+        
+        
+        
+        
     }
 }
